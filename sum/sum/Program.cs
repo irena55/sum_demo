3495d96afdfb5521b048cc949243837ee37c30cc
irena55 <irena55@gmail.com>
2016-11-08 16:51:15 +0200

    sum commit

---
+++ sum/sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 1;
+            Console.Write("The sum 1");
+            while (num < n)
+            {
+                num++;
+                sum += num;
+                Console.Write(" + " + num);
+            }
+            Console.WriteLine(" = " + sum);
+            Console.WriteLine(sum);
+        }
+    }
+}
